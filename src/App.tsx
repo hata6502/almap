c12593b627b2954e229a0865ab2962de481dd02c
hata6502 <7702653+hata6502@users.noreply.github.com>
2024-02-24 16:17:23 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -265,7 +265,7 @@ export const App: FunctionComponent<{
 
         <Popover className="absolute left-24 top-2.5 z-1000">
           <Popover.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-            Options
+            日付選択テスト
             <ChevronDownIcon
               className="-mr-1 h-5 w-5 text-gray-400"
               aria-hidden="true"
