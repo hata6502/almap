db255ce0675cb0d6bf061512cb84f27f18b7ae1b
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2024-05-12 03:20:06 +0900

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -25,7 +25,7 @@ const readme: Photo = {
   // 日本経緯度原点
   latitude: 35.39291572,
   longitude: 139.44288869,
-  originalDate: new Date(-8639999999999999),
+  originalDate: new Date(0),
   url: almapScrapboxURL,
 };
 
